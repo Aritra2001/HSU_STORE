08ccaa1e969c5552402aa9c82ca76658da081d85
Aritra Baidik <aritrabaidik@gmail.com>
2024-03-16 09:58:51 +0530

    razorpay full implementation

--- backend/routes/razorpay.js
+++ backend/routes/razorpay.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const requireAuth = require('../middleware/requireAuth')
-const { razorpayCreateOrder, paypalCreateOrders, paypalExecuteOrders, getHex_Coins } = require('../controllers/razorpayController')
+const { razorpayCreateOrder, paypalCreateOrders, paypalExecuteOrders, getHex_Coins, razorpayVerifyOrder } = require('../controllers/razorpayController')
 
 const router = express.Router()
 
 router.use(requireAuth)
 
 router.post('/create-order', razorpayCreateOrder)
+router.post('/verify-order', razorpayVerifyOrder)
 router.post('/create-ovs-orders', paypalCreateOrders)
 router.post('/create-ovs-execute', paypalExecuteOrders)
 router.get('/get-hex_coins', getHex_Coins)
--- frontend/src/pages/EmailVerified.jsx
+++ frontend/src/pages/EmailVerified.jsx
@@ -7,36 +7,36 @@ import { useNavigate } from 'react-router-dom';
 
 const EmailVerified = () => {
     const [isVerified, setIsVerified] = useState(999);
-    var json = ''
-    const { dispatch } = useAuthContext()
-    const navigator = useNavigate()
+    const jsonRef = useRef('');
+    const { dispatch } = useAuthContext();
+    const navigator = useNavigate();
 
     useEffect(() => {
-      const handleEmailVerified = async () => {
-        try {
-            const token = getToken();
-            const response = await fetch(`https://hsu-store-backend.vercel.app/api/users/verify-email/${token}`, {
-                method: 'GET',
-            });
+        const handleEmailVerified = async () => {
+            try {
+                const token = getToken();
+                const response = await fetch(`https://hsu-store-backend.vercel.app/api/users/verify-email/${token}`, {
+                    method: 'GET',
+                });
 
-            json = await response.json()
-            if (response.ok) {
-                setIsVerified(1);
-                localStorage.setItem('user', JSON.stringify(json))
-                dispatch({type: 'LOGIN', payload: json})
-                setTimeout(() => {
-                  navigator('/')
-                }, 3000)
-            } else {
-                const json = await response.json();
-                console.log(json);
+                const data = await response.json();
+                if (response.ok) {
+                    setIsVerified(1);
+                    jsonRef.current = data; // Store the data in the ref
+                    localStorage.setItem('user', JSON.stringify(data));
+                    dispatch({ type: 'LOGIN', payload: data });
+                    setTimeout(() => {
+                        navigator('/')
+                    }, 3000);
+                } else {
+                    console.log(data);
+                }
+            } catch (error) {
+                console.error('Error:', error);
             }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
-    handleEmailVerified()
-    },[dispatch, json, navigator])
+        };
+        handleEmailVerified();
+    }, [dispatch, navigator]);
 
 
     const getToken = () => {
@@ -45,37 +45,35 @@ const EmailVerified = () => {
     };
 
     if (isVerified === 1) {
-      return (
-        <div className="h-screen w-full">
-            <div className="h-screen flex flex-col justify-center items-center">
-                <div className="flex flex-col justify-center items-center sm:gap-7 gap-6">
-                    <img className="object-center flex sm:w-[80px] w-[50px]" src={GreenCheck} alt="Green Check" />
-                    <div className="text-neutral-500 sm:text-[28px] font-medium font-['Poppins'] sm:leading-[20px] leading-[5px]">
-                        Your email ID has been&nbsp;
+        return (
+            <div className="h-screen w-full">
+                <div className="h-screen flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center sm:gap-7 gap-6">
+                        <img className="object-center flex sm:w-[80px] w-[50px]" src={GreenCheck} alt="Green Check" />
+                        <div className="text-neutral-500 sm:text-[28px] font-medium font-['Poppins'] sm:leading-[20px] leading-[5px]">
+                            Your email ID has been&nbsp;
+                        </div>
+                        <div className="flex flex-col justify-center items-center text-black sm:text-[28px] font-medium font-['Poppins'] leading-[0px]">
+                            verified successfully
+                        </div>
+                        <span className="text-neutral-500 sm:text-[28px] font-medium font-['Poppins'] leading-[72px]"></span>
                     </div>
-                    <div className="flex flex-col justify-center items-center text-black sm:text-[28px] font-medium font-['Poppins'] leading-[0px]">
-                        verified successfully
-                    </div>
-                    <span className="text-neutral-500 sm:text-[28px] font-medium font-['Poppins'] leading-[72px]"></span>
-                </div>
 
-                <div className="max-sm:mt-8">
-                    <span className="text-violet-600 sm:text-xl text-xs font-normal font-['Poppins']">
-                        <a href="/">Return to profile</a>
-                    </span>
+                    <div className="max-sm:mt-8">
+                        <span className="text-violet-600 sm:text-xl text-xs font-normal font-['Poppins']">
+                            <a href="/">Return to profile</a>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className='h-screen flex justify-center items-center font-["Poppins"] sm:text-3xl bg-stone-200 gap-2'>
+                <div className='loader'></div>
+            </div>
+        )
     }
-  else {
-    return (
-      <div className='h-screen flex justify-center items-center font-["Poppins"] sm:text-3xl bg-stone-200 gap-2'>
-        
-        <div className='loader'></div>
-      </div>
-    )
-  }
 };
 
 export default EmailVerified;
