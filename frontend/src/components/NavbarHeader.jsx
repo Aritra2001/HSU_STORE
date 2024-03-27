024672af2ef5641f44a223eb35e92d2a9add58d4
Aritra Baidik <aritrabaidik@gmail.com>
2024-03-27 13:33:18 +0530

    header gap adjustment

--- frontend/src/components/NavbarHeader.jsx
+++ frontend/src/components/NavbarHeader.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation from react-router-dom
-import { useAuthContext } from "../hooks/useAuthContext";
-import Logo from '../assets/Logo.svg';
 import cart from '../assets/cart.svg';
-import { CiSearch } from "react-icons/ci";
 import { AiOutlineSearch } from 'react-icons/ai';
-import { IoPersonCircleOutline } from "react-icons/io5";
 
 const NavbarHeader = () => {
 
-    const { user } = useAuthContext();
-    const location = useLocation(); // Use useLocation hook to get current location
     const [search, setSearch] = useState('');
 
     const [isChecked, setIsChecked] = useState(false);
@@ -19,14 +12,8 @@ const NavbarHeader = () => {
         setIsChecked(!isChecked);
       };
 
-
-    // Function to determine if the link is active
-    /*const isLinkActive = (href) => {
-        return location.pathname === href;
-    };*/
-
     return (
-        <div className='flex items-center flex-row justify-center h-[110px] gap-2 sm:hidden'>
+        <div className='flex items-center flex-row justify-center h-[110px] max-sm:gap-4 sm:hidden'>
         <label className="flex items-center cursor-pointer">
       <div className='relative flex flex-col items-center'>
       <div className='font-["inter"] font-bold text-[10px]'>Space store</div>
@@ -51,7 +38,7 @@ const NavbarHeader = () => {
                             value={search} 
                             onChange={(e) => setSearch(e.target.value)} 
                             placeholder='search in store' 
-                            className='w-[214px] h-[29px] pl-10 pr-10 bg-gray-200 rounded-[11px] border border-black text-zinc-500 text-xs font-medium placeholder:indent-[0.5rem] indent-[0.5rem]'
+                            className='w-[214px] h-[29px] pl-10 pr-10 bg-gray-200 rounded-[11px] border border-black text-zinc-500 text-xs font-medium placeholder:indent-[0.3rem] indent-[0.3rem] font-["inter"]'
                         />
                         <button className='flex justify-center items-center absolute top-[50%] right-[0.5px] transform -translate-y-1/2 cursor-pointer bg-white w-11 h-7 shadow rounded-[11px]'><AiOutlineSearch size={20} color='#000000' className=''/></button>
                     </div>
